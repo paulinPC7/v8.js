1078322e01c0a14ab9747cc96ecf2d728b9e6f5b
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-04-23 10:24:08 -0300

    Update fatorial.js

--- fatorial.js
+++ fatorial.js
@@ -29,3 +29,26 @@ let resultado = 0;
 resultado = fatorial * (fatorial-1) * (fatorial-2) * (fatorial-3) * (fatorial-4) * (fatorial-5) * (fatorial-6)* (fatorial-7)* (fatorial-8)* (fatorial-9);
 console.log("o resultado é:" + resultado);
 
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+  const fatorial = 30;
+let i = 0;
+let resultado = 0;
+
+resultado = fatorial;
+console.log(resultado + "!");
+for (i = 1; i < fatorial; i++ ) {
+
+    console.log(resultado + " = ("+resultado+" * ( "+ 
+                
+    fatorial+ " - " + i + ")");
+    resultado = resultado * (fatorial - i);
+}
