4e4a10c85dd9370a8292d3b581669fc163835b36
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-04-23 08:49:09 -0300

    Create idade.js

---
+++ idade.js
@@ -0,0 +1,12 @@
+const idade= 15;
+
+if(idade>=18){
+    console.log("sua idade é: " +
+                idade+
+                ", você é maior de idade"); 
+                idade>=18?"maiordeidade" :"menor de idade";
+} else {
+    console.log("sua idade é: " +
+                idade+
+                ",você é menor de idade");
+}
