199b611cd46a49c30fd64d333cb77982d1f213b5
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-05-22 09:00:37 -0300

    Create função.js

---
+++ função.js
@@ -0,0 +1,15 @@
+console.log("resultado", soma());
+console.log("resultado2", soma2(5, 7));
+x = 5;
+x = x + 1;
+
+function soma() {
+    x = 5;
+    x = x + 1;
+    return x;
+}
+
+function soma2(a,b){
+    r = a + b;
+    return r;
+}
