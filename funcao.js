0bcd7a315dc25fc2af9070e852721de0e2e6e72b
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-05-28 09:41:18 -0300

    Update funcao.js

--- funcao.js
+++ funcao.js
@@ -1,12 +1,24 @@
-console.log (soma (5,9));
-function soma (v1, v2){
+console.log (multiplicação (1358,86));
+function multiplicação (v1, v2){
+    return v1*v2;
+}
+
+console.log (soma (10,8));
+function soma (v1=0, v2=0){
     return v1 + v2;
 
 }
-function nomeidade (nome,idade){
-    console.log(`nome: ${nome}-idade:${idade}`);
+function nomeidade (nome="",idade=""){
+    console.log(`nome: ${nome}-idade: ${idade}`);
 }
 nomeidade("Paulo","18")
+
+nomeidade("18","Paulo")
+
+console.log(soma(9));
+nomeidade("Sidney");
+
+
 
 
 
