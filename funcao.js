34b267719c1bb98b64ef2166533a2143bc45edf0
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-05-28 09:08:38 -0300

    Create funcao.js

---
+++ funcao.js
@@ -0,0 +1,13 @@
+console.log (soma (5,9));
+function soma (v1, v2){
+    return v1 + v2;
+
+}
+function nomeidade (nome,idade){
+    console.log(`nome: ${nome}-idade:${idade}`);
+}
+nomeidade("Paulo","18")
+
+
+
+
