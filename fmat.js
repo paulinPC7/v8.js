591340bbbba38df3642afb8a074c47c0d10691ee
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-06-04 09:19:37 -0300

    Create fmat.js

---
+++ fmat.js
@@ -0,0 +1,22 @@
+console.log (multiplicação (1358,86));
+function multiplicação (v1, v2){
+    return v1*v2;
+}
+
+console.log (soma (10,8));
+function soma (v1=0, v2=0){
+    return v1 + v2;
+
+}
+
+console.log (subtração (10,8));
+function subtração (v1=0, v2=0){
+    return v1 - v2;
+
+}
+
+console.log (divisão (10,8));
+function divisão (v1=0, v2=0){
+    return v1 / v2;
+
+}
