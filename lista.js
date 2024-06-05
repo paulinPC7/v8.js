aa7533ea9e7348d1f5a4dcc3ebd62928d7374277
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-06-05 09:03:35 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,1 +1,5 @@
 
+const produtos=["cloro","amaciante","detergente","sabonte","sabão","sabão em pedra","sabão em pó","sabão liquido","sabonete liquido","agua sanitaria","pasta de dente","esponja","alcool","alcool perfumado",]
+console.log(produtos);
+console.log(produtos[2]);
+console.log(produtos.length);
