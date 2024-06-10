9f3d9d68f23af3fa17164584558085c7adf90466
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-06-10 12:35:10 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -3,3 +3,16 @@ const produtos=["cloro","amaciante","detergente","sabonte","sabão","sabão em p
 console.log(produtos);
 console.log(produtos[2]);
 console.log(produtos.length);
+
+produtos.push("cloro")
+produtos.pop( )
+nprodutos = produtos.splice(3,4)
+console.log(nprodutos)
+produtos1=produtos.slice(0,5)
+produtos2=produtos.slice(5)
+console.log(produtos1)
+console.log(produtos2)       
+produtos1.push(" ")
+produtos2.push(" ")
+todosprodutos = produtos1.concat(produtos2)
+console.log(todosprodutos)
